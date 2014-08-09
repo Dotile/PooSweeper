dc5022de452b65bbf61f912069ef6a86ea2690e3
Dotile <dominikleclerc@gmail.com>
2014-08-09 20:32:09 +0200

    Added the getCellInfo function

--- PooSweeperState.cpp
+++ PooSweeperState.cpp
@@ -25,3 +25,47 @@ void PooSweeperState::initialize(size_t numRows, size_t numCols, size_t numPoos)
     }
   }
 }
+
+
+// _____________________________________________________________________________
+CellInfo PooSweeperState::getCellInfo(size_t rowIndex, size_t colIndex) {
+ // Look for mines in sorrounding fields using if Statements
+ int CellInfo = 0;
+ // Center
+ if(_pooField[rowIndex][colIndex] == POO) {
+   CellInfo = REVEALED_POO;
+ }
+ // Upper left
+ if(_pooField[rowIndex - 1][colIndex - 1 == POO) {
+   CellInfo++;
+ }
+ // Upper middle
+ if(_pooField[rowIndex - 1][colIndex] == POO) {
+   CellInfo++;
+ }
+ // Upper right
+ if(_pooField[rowIndex - 1][colIndex + 1] == POO) {
+   CellInfo++;
+ }
+ // Center left
+ if(_pooField[rowIndex][colIndex - 1] == POO) {
+   CellInfo++;
+ }
+ // Center right
+ if(_pooField[rowIndex][colIndex + 1] == POO) {
+   CellInfo++;
+ }
+ // Lower left
+ if(_pooField[rowIndex + 1][colIndex - 1] == POO) {
+   CellInfo++;
+ }
+ // Lower middle 
+ if(_pooField[rowIndex + 1][colIndex] == POO) {
+   CellInfo++;
+ }
+ // Lower right 
+ if(_pooField[rowIndex + 1][colIndex] == POO) {
+   CellInfo++;
+ }
+ return CellInfo;
+}
