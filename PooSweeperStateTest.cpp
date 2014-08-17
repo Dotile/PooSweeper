e2473753cebe8bbf3b89fb874b517a4b39468371
Dotile <dominikleclerc@gmail.com>
2014-08-17 22:02:02 +0200

    added Test (not working)

---
+++ PooSweeperStateTest.cpp
@@ -0,0 +1,9 @@
+// Copyright 2014
+// Dominik Leclerc 
+
+#include <gtest/gtest.h>
+#include "./PooSweeperState.h"
+
+// ____________________________________________________________________________
+TEST(StateTest, initialize) {
+
