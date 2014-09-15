3e7be779b541a1efdd32c804ef0bed9092d5819e
Dotile <dominikleclerc@gmail.com>
2014-09-15 10:44:54 +0200

    worked on non working Tests

--- PooSweeperStateTest.cpp
+++ PooSweeperStateTest.cpp
@@ -4,20 +4,28 @@
 #include <gtest/gtest.h>
 #include "./PooSweeperState.h"
 
-// ____________________________________________________________________________
+// _________________________________________________________________________
 TEST(PooSweeperStateTest, initialize) {
-  PooSweeperState pss;
-  pss.initialize(10, 10, 10);
+  PooSweeperState pssa;
+  pssa.initialize(10, 10, 10);
   // Is the quantity of bombs correct?
   size_t count = 0;
   for (int i = 0; i < 10; ++i) {
     for (int j = 0; j < 10; ++j) {
-      if (pss._pooField[i][j] == PooSweeperState::POO) {
+      if (pssa._pooField[i][j] == PooSweeperState::POO) {
         ++count;
       }
     }
   }
   ASSERT_EQ(10, count);
-  ASSERT_EQ(10, pss.numRows());
-  ASSERT_EQ(10, pss.numCols());
+  ASSERT_EQ(10, pssa.numRows());
+  ASSERT_EQ(10, pssa.numCols());
 }
+
+// ____________________________________________________________________________
+TEST(PooSweeperStateTest, initialize2) {
+  PooSweeperState pssb;
+  pssb.initialize(10, 10, 10);
+  ASSERT_EQ(10, pssb.numRows());
+}
+
