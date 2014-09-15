8fbb1db171870fcd77c485a2c7ae5f03fc799a23
Dotile <dominikleclerc@gmail.com>
2014-09-15 19:04:27 +0200

    pasted Test in Test.cpp

--- PooSweeperStateTest.cpp
+++ PooSweeperStateTest.cpp
@@ -22,10 +22,68 @@ TEST(PooSweeperStateTest, initialize) {
   ASSERT_EQ(10, pssa.numCols());
 }
 
-// _________________________________________________________________________
-TEST(PooSweeperStateTest, numRows) {
-  PooSweeperState pssb;
-  pssb.numRows();
-  ASSERT_EQ(5, pssb._numRows);
+
+// ________________________________________________________________________
+TEST(PooSweeperStateTest, applyMove) {
+  {
+    // No poos
+    PooSweeperState pss;
+    pss._numRows = 5;
+    pss._numCols = 5;
+    pss._numPoos = 0;
+    pss._numRevealed = 0;
+    pss._numMarked = 0;
+    pss._status = PooSweeperStateBase::ONGOING;
+    pss.CellInfoStorage.clear();
+    pss.CellInfoPoo.clear();
+    pss.CellInfoStorage.resize(5);
+    pss.CellInfoPoo.resize(5);
+    for (int i = 0; i < 5; ++i) {
+      for (int j = 0; j < 5; ++j) {
+        pss.CellInfoStorage[i].push_back(PooSweeperStateBase::UNREVEALED);
+        pss.CellInfoPoo[i].push_back(PooSweeperState::NO_POO);
+      }
+    }
+    PooSweeperMove move;
+    move.col = 0;
+    move.row = 0;
+    move.type = PooSweeperMove::REVEAL;
+    pss.applyMove(move);
+    for (int i = 0; i < 5; ++i) {
+      for (int j = 0; j < 5; ++j) {
+        ASSERT_EQ(0, pss.CellInfoStorage[i][j]);
+      }
+    }
+  }
+  {
+    // Only poos
+    PooSweeperState pss;
+    pss._numRows = 5;
+    pss._numCols = 5;
+    pss._numPoos = 25;
+    pss._numRevealed = 0;
+    pss._numMarked = 0;
+    pss._status = PooSweeperStateBase::ONGOING;
+    pss.CellInfoStorage.clear();
+    pss.CellInfoPoo.clear();
+    pss.CellInfoStorage.resize(5);
+    pss.CellInfoPoo.resize(5);
+    for (int i = 0; i < 5; ++i) {
+      for (int j = 0; j < 5; ++j) {
+        pss.CellInfoStorage[i].push_back(PooSweeperStateBase::UNREVEALED);
+        pss.CellInfoPoo[i].push_back(PooSweeperState::POO);
+      }
+    }
+    PooSweeperMove move;
+    move.col = 0;
+    move.row = 0;
+    move.type = PooSweeperMove::REVEAL;
+    pss.applyMove(move);
+    for (int i = 0; i < 5; ++i) {
+      for (int j = 0; j < 5; ++j) {
+        ASSERT_EQ(-3, pss.CellInfoStorage[i][j]);
+      }
+    }
+  }
 }
 
